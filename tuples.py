880824633601a34377ea6cf9fe5f611a2cdd7fc5
shreevaths <srivathsa.kalyan@gmail.com>
2023-05-02 20:42:51 +0530

    added more methods on tuples

--- tuples.py
+++ tuples.py
@@ -1,3 +1,12 @@
 coordinates = (4, 5)
 # coordinates[1] = 10
 print(coordinates[1])
+
+t1 = (45, 5, 67, 0, 1, 4, 5)
+# print(t1.count(1))
+print(sorted(t1))
+
+# Nested tuple
+NT = (1, 2, ("pop", "rock"), (3, 4), ("disco", (1, 2)))
+print(NT[4][1][1])
+print(NT.index(("pop", "rock")))
