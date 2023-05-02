a0140419f3129baaf328544f2fe4e371624ca66f
shreevaths <srivathsa.kalyan@gmail.com>
2023-05-02 20:43:14 +0530

    tested changed programs

---
+++ app.py
@@ -0,0 +1,9 @@
+a = "Hello! Michael Jackson has: 12 characters."
+print(a)
+b = a.replace("!", ",").replace(":", ",").replace(".", " ")
+print(b)
+
+print(b.index("e"))
+print(b.find("e"))
+# print(b.index("13"))
+print(b.find("13"))
